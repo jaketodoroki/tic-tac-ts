58eb3d3cf859b65349732851cb47631e8f788c7b
JakeTodoroki <taco7897@gmail.com>
2023-02-21 12:48:31 -0700

    event listeners and init, render, handle click function

--- js/app.js
+++ js/app.js
@@ -15,7 +15,37 @@ let board;
 let turn;
 let winner;
 let tie;
-//-------Cached Element References----
+//-------Cached Element References---------
 const squareEls = document.querySelectorAll('.sqr');
 const messageEl = document.getElementById('message');
 const resetBtnEl = document.querySelector('#reset-button');
+const boardEl = document.querySelector('.board');
+//--------Event Listeners--------
+resetBtnEl.addEventListener('click', init);
+boardEl.addEventListener('click', handleClick);
+//--------Functions-----------
+function init() {
+    board = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    turn = 1;
+    winner = false;
+    tie = false;
+    render();
+}
+function render() {
+    // updateBoard()
+    // updateMessage()
+}
+function handleClick(evt) {
+    if (!(evt.target instanceof HTMLElement))
+        return;
+    if (evt.target.textContent !== "")
+        return;
+    if (winner === true || tie === true)
+        return;
+    let sqIdx = parseInt(evt.target.id.slice(2));
+    // placePiece(sqIdx)
+    // checkForTie()
+    // checkForWinner()
+    // switchPlayerTurn()
+    render();
+}
--- src/app.ts
+++ src/app.ts
@@ -18,8 +18,45 @@ let turn: number
 let winner: boolean
 let tie: boolean
 
-//-------Cached Element References----
+//-------Cached Element References---------
 
 const squareEls = document.querySelectorAll<HTMLButtonElement>('.sqr')
 const messageEl = document.getElementById('message') as HTMLElement
 const resetBtnEl = document.querySelector<HTMLButtonElement>('#reset-button')!
+const boardEl = document.querySelector<HTMLDivElement>('.board')!
+
+
+//--------Event Listeners--------
+
+resetBtnEl.addEventListener('click', init)
+
+boardEl.addEventListener('click', handleClick)
+
+
+//--------Functions-----------
+
+function init(): void {
+  board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  turn = 1
+  winner = false
+  tie = false
+  render()
+}
+
+function render() {
+  // updateBoard()
+  // updateMessage()
+}
+
+
+function handleClick(evt: MouseEvent): void {
+  if(!(evt.target instanceof HTMLElement)) return
+  if (evt.target.textContent !== "") return
+  if(winner === true || tie === true) return
+  let sqIdx: number = parseInt(evt.target.id.slice(2))
+  // placePiece(sqIdx)
+  // checkForTie()
+  // checkForWinner()
+  // switchPlayerTurn()
+  render()
+}
