61c710584e7b0c6406f458c72e9577b6618a4666
JakeTodoroki <taco7897@gmail.com>
2023-02-21 11:54:11 -0700

    html and css

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+//---------Constants---------------
+
+const winningCombos: number[][] = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+//-----------Variables------------
+
+let board: number[]
+let turn: number
+let winner: boolean
+let tie: boolean
+
+//-------Cached Element References----
+
+const squareEls = document.querySelectorAll<HTMLButtonElement>('.sqr')
